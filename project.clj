29cbedd02acab674991d74572ba63393f4709c62
Yrjö Kari-Koskinen <ykk@peruna.fi>
2018-02-28 23:34:30 +0200

    clean up project.clj and fix dependencies to make hugsql compile into a war o_O

--- project.clj
+++ project.clj
@@ -3,17 +3,15 @@
   :url "http://clojure-getting-started.herokuapp.com"
   :license {:name "Eclipse Public License v1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.4.0"]
-                 [ring/ring-jetty-adapter "1.4.0"]
+                 [ring/ring-jetty-adapter "1.6.3"]
                  [environ "1.0.0"]
                  [com.layerware/hugsql "0.4.8"]
+                 [org.clojure/tools.reader "1.2.1"]
                  [org.postgresql/postgresql "42.2.1"]
                  [hiccup "1.0.5"]]
-  :plugins [[lein-ring "0.8.13"]
-            [environ/environ.lein "0.3.1"]]
+  :plugins [[lein-ring "0.8.13"]]
   :ring {:handler clojure-getting-started.web/app}
-  :min-lein-version "2.0.0"
-  :hooks [environ.leiningen.hooks]
-  :uberjar-name "clojure-getting-started-standalone.jar"
-  :profiles {:production {:env {:production true}}})
+  :profiles
+   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]]}})
